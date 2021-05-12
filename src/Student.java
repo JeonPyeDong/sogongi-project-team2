42198bff8b33e70c39b48d7f0ac9d0d2bf3b8e80
yunseong9999 <dbsdltjd3701@naver.com>
2021-05-13 02:06:11 +0900

    modify studentPasswordChange()

--- src/Student.java
+++ src/Student.java
@@ -5,72 +5,82 @@ public class Student {
 	private String studentName;
 	private String[] studentSubject = new String[6];
 	private String studentPassword;
-	
+
 	public Student(String id, String name, String[] subject) {
 		this.studentPassword = "1234";
 		this.studentId = id;
 		this.studentName = name;
 		this.studentSubject = subject;
 	}
+
 	public String getStudentId() {
 		return studentId;
 	}
+
 	public String getStudentName() {
 		return studentName;
 	}
+
 	public String[] getStudentSubjects() {
 		return studentSubject;
 	}
+
 	public String getStudentPassword() {
 		return studentPassword;
 	}
+
 	public void setStudentId(String id) {
 		this.studentId = id;
 	}
+
 	public void setStudentName(String name) {
 		this.studentName = name;
 	}
+
 	public void setStudentSubjects(String[] subject) {
 		this.studentSubject = subject;
 	}
+
 	public void setStudentPassword(String password) {
 		this.studentPassword = password;
 	}
-	//학생 비밀번호 변경 메소드
+
+	// 학생 비밀번호 변경 메소드
 	public void studentPasswordChange() {
 		System.out.println("-----------------");
 		Scanner scan = new Scanner(System.in);
 		System.out.print("새로운 비밀번호를 입력하시오.>> ");
 		String password1 = scan.next();
-		if(studentPassword == password1) {
+		if (password1.equals(getStudentPassword())) {
 			System.out.println("중복된 비밀번호입니다.");
 			studentPasswordChange();
-		}
-		else {
-			while(true) {
+		} else {
+			while (true) {
 				System.out.print("한번 더 입력하세요. >> ");
 				String password2 = scan.next();
-				if(password1 != password2) {
+				if (!password1.equals(password2)) {
 					setStudentPassword(password2);
-					System.out.println("비밀번호가 변경되었습니다."+password2);
+					System.out.println("비밀번호가 변경되었습니다." + password2);
 					break;
 				}
 			}
 		}
 		studentLogin();
 	}
-	//학생 본인이 수강중인 수업 열람 메소드.
+
+	// 학생 본인이 수강중인 수업 열람 메소드.
 	public void showStudent() {
 		System.out.println("-----------------");
-		System.out.println("학번: "+studentId+"  이름: "+studentName);
+		System.out.println("학번: " + studentId + "  이름: " + studentName);
 		System.out.print("수강과목: ");
 		System.out.print("[ ");
-		for(int i = 0; i < studentSubject.length; i++)
-			System.out.print(studentSubject[i]+" ");
+		for (int i = 0; i < studentSubject.length; i++)
+			System.out.print(studentSubject[i] + " ");
 		System.out.println("]");
 		studentLogin();
 	}
-	//학생으로 로그인 했을 때 선택.
+
+	// 학생으로 로그인 했을 때 선택.
 	public void studentLogin() {
 		System.out.println("-----------------");
 		System.out.println("1. 비밀번호 변경");
@@ -78,26 +88,27 @@ public class Student {
 		System.out.print("입력 >> ");
 		Scanner scan = new Scanner(System.in);
 		int input = scan.nextInt();
-		switch(input) {
-		case 1:        //학생 비밀번호 변경
-			studentPasswordChange();
-			break;
-		case 2:        //학생 본인이 수강중인 수업 열람
-			showStudent();
-			break;
-		default:
-			System.out.println("잘못된 입력입니다.");
-			studentLogin();
+		switch (input) {
+			case 1: // 학생 비밀번호 변경
+				studentPasswordChange();
+				break;
+			case 2: // 학생 본인이 수강중인 수업 열람
+				showStudent();
+				break;
+			default:
+				System.out.println("잘못된 입력입니다.");
+				studentLogin();
 		}
 	}
-	//studentId를 전달 받으면 전체 학생배열에서 중복되는 학번이 있는지 찾는 메소드.
+
+	// studentId를 전달 받으면 전체 학생배열에서 중복되는 학번이 있는지 찾는 메소드.
 	public boolean studentIdCheck(String id) {
-		boolean check = false; 
-		for(int i = 0; i < student.length; i++) {
-			if(student[i].studentId == id)
+		boolean check = false;
+		for (int i = 0; i < student.length; i++) {
+			if (student[i].studentId == id)
 				check = true;
 		}
 		return check;
 	}
-  
+
 }
