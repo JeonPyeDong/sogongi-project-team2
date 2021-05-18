db0baece4b5ce6ffc45b914cf3e31965302aab0a
yunseong9999 <dbsdltjd3701@naver.com>
2021-05-18 13:44:44 +0900

    modify coding covention on vsCode

--- src/Main.java
+++ src/Main.java
@@ -1,56 +1,18 @@
 import java.util.Scanner;
 
 public class Main {
-	public static void main(String[] args) {
-		
-		int exitFlag = 0;
-		int saveFlag = 0; // 저장 후 변경이 있는지 확인하는 변수 0이면 변경후 미저장
-		Professor whoPro = new Professor(null, null, null);
-		Student whoStu = new Student(null, null, args);
-		Scanner s = null;
-		try {
-			s = new Scanner(System.in);
-			while (exitFlag == 0) {
-				printMenu(); // 메뉴 출력
-				int input = s.nextInt();
-				switch (input) {
-				case 1:
-					//logInToProfessor(s);
-					logProfessor(whoPro);
-					System.out.println(whoPro.getName() + "교수님, 환영합니다!");
-					saveFlag = 0;
-					break;
-				case 2:
-					//logInToStudent(s);
-					//logStudent();
-					saveFlag = 0;
-					break;
-				case 3:
-					professorAdd();
-					break;
-				case 0:
-					if (saveFlag == 0) {
-						System.out.println("변경 후 저장되지 않았습니다. 정말 종료하시겠습니까? (y/n)");
-						String answer = s.next();
-						if (answer.equals("y"))
-							exitFlag = 1; // 정말 종료
-						else {
-							System.out.println("종료되지 않았습니다.");
-							continue;
-						}
-					} else
-						exitFlag = 1;
-					break;
-				default:
-					System.out.println("메뉴에 있는 번호를 입력해주세요.");
-				}
-			}
-		} catch (Exception e) {
-			System.out.println("예외 발생(프로그램 종료)");
-		} finally {
-			s.close();
-		}
+	public static Professor[] professor = new Professor[6];
+	public static int professorCount = 0;
 
+	// 초기 교수배열을 초기화하는 메소드. 테스트 용임. professorAdd()가 구현된다면 삭제될 메소드.
+	static void setUpProfessor() {
+		String initpasswd = "1234";
+		professor[0] = new Professor("정희용", "선형대수학", initpasswd);
+		professor[1] = new Professor("김경백", "리눅스시스템", initpasswd);
+		professor[2] = new Professor("이귀상", "논리회로", initpasswd);
+		professor[3] = new Professor("김승원", "소프트웨어공학기초프로젝트", initpasswd);
+		professor[4] = new Professor("강대욱", "자바프로그래밍", initpasswd);
+		professor[5] = new Professor("임형석", "이산수학", initpasswd);
 	}
 
 	static void printMenu() {
@@ -62,53 +24,18 @@ public class Main {
 		System.out.print("입력 >> ");
 	}
 
-/*
-	static void logInToProfessor(Scanner s) {
-		String name;
-		String password;
-		System.out.println("-- 교수로 로그인 --");
-		System.out.println("이름을 입력하세요.");
-		System.out.print("입력 >> ");
-		name = s.next();
-		if (Professor.Professor(name)) {
-			System.out.println("비밀번호를 입력하세요.");
-			System.out.print("입력 >> ");
-			password = s.next();
-			if (Professor.Professor(password)) {
-				System.out.println("교수님 환영합니다!");       // 교수 이름 불러와서 이름 000교수님 환영합니다 될 수 있게 하고 싶엉
-				// 무엇을 보게 하고 싶은지 출력
- 			}
-			else
-				System.out.println("E : 비밀번호가 틀렸습니다.");
-		}
-		else
-			System.out.println("E : 존재하지 않는 교수입니다.");
+	// 해당 과목의 입력은 안받을지??
+	public static void professorAdd() {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("교수로 등록할 이름을 입력하세요.");
+		String name = sc.nextLine();
+		System.out.println("비밀번호를 입력하세요.");
+		String pw = sc.nextLine();
+		professor[professorCount++] = new Professor(name, pw);
 	}
-	
-	static void logInToStudent(Scanner s) {
-		String id;
-		String passwd;
-		System.out.println("-- 학생으로 로그인 --");
-		System.out.println("학번을 입력하세요");
-		System.out.println("입력 >> ");
-		id = s.next();
-		if (Student.student(id)) {
-			System.out.println("비밀번호를 입력하세요.");
-			System.out.println("입력 >> ");
-			passwd = s.next();
-			if (Student.student(passwd)) {
-				System.out.println("학우님 환영합니다!");         //학생 이름 불러와서 000학우님 환영합니다 이렇게 하고 싶음 마찬가지로
-				// 수강과목 출력
-			}
-			else
-				System.out.println("E : 비밀번호가 틀렸습니다.");
-		}
-		else
-			System.out.println("E : 존재하지 않는 학생입니다.");
-	}
-*/
 
-	private static Professor logProfessor(Professor whoPro){
+	// whoPro가 메인메소드의 whopro를 뜻하는지?
+	private static Professor logProfessor(Professor whoPro) {
 		Scanner sc = new Scanner(System.in);
 		String logid;
 		String logpw;
@@ -119,57 +46,101 @@ public class Main {
 		logid = sc.nextLine();
 		System.out.println("비밀번호를 입력하세요 >> ");
 		logpw = sc.nextLine();
- 
-		for(Professor i : professor){
-			if(logid.equals(i.getName())) idpass = true;
-			if(logpw.equals(i.getPassword())) pwpass = true;
+
+		for (Professor i : professor) {
+			if (logid.equals(i.getName()))
+				idpass = true;
+			if (logpw.equals(i.getPassword()))
+				pwpass = true;
 			whoPro = i;
 			break;
 		}
 
-		if(idpass && pwpass) {
-			System.out.println("로그인 성공!");	
+		if (idpass && pwpass) {
+			System.out.println("로그인 성공!");
 		}
 		return whoPro;
- } 			// logProfessor() 끝
+	} // logProfessor() 끝
 
-/*
-private static Student logStudent(){
-Student[] ST;
-ST = new Student[];
-Scanner sc = new Scanner(System.in);
-String logid;
-String logpw;
-boolean idpass = false;
-boolean pwpass = false;
-System.out.println("ID를 입력하세요 >> ");
-logid = sc.nextLine();
-System.out.println("비밀번호를 입력하세요 >> ");
-logpw = sc.nextLine();
- 
-		for(Professor i : PF){
-			if(logid.equals(i.getStudentId())) idpass = true;
-			if(logpw.equals(i.getStudentPassword())) pwpass = true;
-			whoStu = i;
-			break;
+	public static void main(String[] args) {
+
+		int exitFlag = 0;
+		int saveFlag = 0; // 저장 후 변경이 있는지 확인하는 변수 0이면 변경후 미저장
+		setUpProfessor();
+		Professor whoPro = new Professor(null, null, null);
+		Student whoStu = new Student(null, null, args);
+		Scanner s = null;
+		try {
+			s = new Scanner(System.in);
+			while (exitFlag == 0) {
+				printMenu(); // 메뉴 출력
+				int input = s.nextInt();
+				switch (input) {
+					case 1:
+						// logInToProfessor(s);
+						logProfessor(whoPro);
+						System.out.println(whoPro.getName() + "교수님, 환영합니다!");
+						saveFlag = 0;
+						break;
+					case 2:
+						// logInToStudent(s);
+						// logStudent();
+						saveFlag = 0;
+						break;
+					case 3:
+						professorAdd();
+						break;
+					case 0:
+						if (saveFlag == 0) {
+							System.out.println("변경 후 저장되지 않았습니다. 정말 종료하시겠습니까? (y/n)");
+							String answer = s.next();
+							if (answer.equals("y"))
+								exitFlag = 1; // 정말 종료
+							else {
+								System.out.println("종료되지 않았습니다.");
+								continue;
+							}
+						} else
+							exitFlag = 1;
+						break;
+					default:
+						System.out.println("메뉴에 있는 번호를 입력해주세요.");
+				}
+			}
+		} catch (Exception e) {
+			System.out.println("예외 발생(프로그램 종료)");
+		} finally {
+			s.close();
 		}
-	if(idpass && pwpass) {
-	System.out.println("로그인 성공!");	
-	}
-	return whoStu;
- }			 // logStudent() 끝 
- */
 
-	public static Professor[] professor = new Professor[6];
-	public static int professorCount = 0;
-	
-	public static void professorAdd() {
-		Scanner sc = new Scanner(System.in);
-		System.out.println("교수로 등록할 이름을 입력하세요.");
-		String name = sc.nextLine();
-		System.out.println("비밀번호를 입력하세요.");
-		String pw = sc.nextLine();
-		professor[professorCount++] = new Professor(name, pw);
-}
+	}
+	// static void logInToProfessor(Scanner s) { String name; String password;
+	// System.out.println("-- 교수로 로그인 --"); System.out.println("이름을 입력하세요.");
+	// System.out.print("입력 >> "); name = s.next(); if (Professor.Professor(name)) {
+	// System.out.println("비밀번호를 입력하세요."); System.out.print("입력 >> "); password =
+	// s.next(); if (Professor.Professor(password)) {
+	// System.out.println("교수님 환영합니다!"); // 교수 이름 불러와서 이름 000교수님 환영합니다 될 수 있게 하고 싶엉
+	// // 무엇을 보게 하고 싶은지 출력 } else System.out.println("E : 비밀번호가 틀렸습니다."); } else
+	// System.out.println("E : 존재하지 않는 교수입니다."); }
+
+	// static void logInToStudent(Scanner s) { String id; String passwd;
+	// System.out.println("-- 학생으로 로그인 --"); System.out.println("학번을 입력하세요");
+	// System.out.println("입력 >> "); id = s.next(); if (Student.student(id)) {
+	// System.out.println("비밀번호를 입력하세요."); System.out.println("입력 >> "); passwd =
+	// s.next(); if (Student.student(passwd)) { System.out.println("학우님 환영합니다!");
+	// //학생 이름 불러와서 000학우님 환영합니다 이렇게 하고 싶음 마찬가지로 // 수강과목 출력 } else
+	// System.out.println("E : 비밀번호가 틀렸습니다."); } else
+	// System.out.println("E : 존재하지 않는 학생입니다."); }
+
+	// private static Student logStudent(){ Student[] ST; ST = new Student[];
+	// Scanner sc = new Scanner(System.in); String logid; String logpw; boolean
+	// idpass = false; boolean pwpass = false; System.out.println("ID를 입력하세요 >> ");
+	// logid = sc.nextLine(); System.out.println("비밀번호를 입력하세요 >> "); logpw =
+	// sc.nextLine();
+
+	// for(Professor i : PF){ if(logid.equals(i.getStudentId())) idpass = true;
+	// if(logpw.equals(i.getStudentPassword())) pwpass = true; whoStu = i; break; }
+	// if(idpass && pwpass) { System.out.println("로그인 성공!"); } return whoStu; } //
+	// logStudent() 끝
 
 }
